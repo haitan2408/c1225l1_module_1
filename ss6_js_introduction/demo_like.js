6b7a0f725590d05d244577f312094a9d02f52f98
haitan2408 <haitan28102408@gmail.com>
2026-01-18 15:39:54 +0700

    haitt up

---
+++ ss6_js_introduction/demo_like.js
@@ -0,0 +1,14 @@
+// người tạo
+// Nội  dung của đoạn code bên dưới (function)
+// Tham số và trả về của function có ý nghĩa gì.
+document.getElementById("btn-like").addEventListener("click", function () {
+    this.style.backgroundColor = "blue";
+    this.style.color = "white";
+    this.innerText = "Liked"
+});
+/*
+document.getElementById("btn-comment").addEventListener("click", function () {
+
+})
+*/
+// nth-child
